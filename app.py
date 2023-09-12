87f06924a3818fbee88956663d9ac3b971fe44cb
Tim <timothywang@yahoo.com>
2023-09-11 23:29:08 -0500

    added functions to detect certain positions of the lift

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import torch
 print(f'PyTorch version: {torch.__version__}')
 print("Is CUDA enabled?",torch.cuda.is_available())
 print('*'*10)
-print(f'_CUDA version: ')
 #!nvcc --version
 print('*'*10)
 print(f'CUDNN version: {torch.backends.cudnn.version()}')
@@ -26,14 +25,15 @@ from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2 import model_zoo
-from PIL import Image 
-import PIL 
+# from PIL import Image 
+# import PIL 
 import cv2
 import numpy as np
-import tqdm
+# import tqdm
 import random
 import matplotlib.pyplot as plt
 import os
+import json
 # %matplotlib inline
 # %%
 videoPath = 'Li Dayin 180kg Snatch WR  216kg Clean&Jerk  2023 AWC in Jinju.mp4'
@@ -144,7 +144,7 @@ cfg_obj.MODEL.WEIGHTS ='output/model_final.pth'
 cfg_obj.DATASETS.TEST = ("barbell_test", )
 cfg_obj.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
 # predictor = DefaultPredictor(cfg_obj)
-test_metadata = MetadataCatalog.get("barbell_test")
+barbell_metadata = MetadataCatalog.get("barbell_test")
 
 # from detectron2.utils.visualizer import ColorMode
 # import glob
@@ -166,18 +166,30 @@ test_metadata = MetadataCatalog.get("barbell_test")
 # outputs_obj = predictor_obj(frame)
 # outputs_obj
 # %%
-def detect(videoPath, config, config_type, metadata, single_frame=False, write=False):
+cfg_key = get_cfg() # obtain detectron2's default config
+# may need to download config file locally for greater speed
+cfg_key.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")) # load values from a file specified under Detectron2's official configs/ directory
+cfg_key.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml") # returns the URL to the model trained using the given config
+cfg_key.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 # threshold to filter out low-scored bounding boxes
+cfg_key.MODEL.DEVICE = 'cuda'  # cpu or cuda
+keypoint_metadata = MetadataCatalog.get(cfg_key.DATASETS.TRAIN[0])
+#%% 
+def keypoint_degree(a,b,c):
+  ba = a - b
+  bc = c-b 
+  cosine_angle = np.dot(ba,bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+  angle = np.arccos(cosine_angle)
+  return np.degrees(angle)
+# %%
+def detect(videoPath):
   # get video and FPS
   video = cv2.VideoCapture(videoPath)
   fps = round(video.get(cv2.CAP_PROP_FPS)) # frame rate in video
   num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) # number of frames in video
-  predictor = DefaultPredictor(config)
-  # if single_frame:
-  #   frame_lim = list(range(num_frames, num_frames+1))
-  # else:
-  #   frame_lim = range(0, num_frames, fps)
+
   def lift_startend(find_start=False):
     global frame_num
+    predictor = DefaultPredictor(cfg_obj)
     # iterate through video every second
     if find_start:
       start_frame = 0
@@ -185,7 +197,7 @@ def detect(videoPath, config, config_type, metadata, single_frame=False, write=F
       start_frame = frame_num
     range_frames = range(start_frame, num_frames, fps)
     for i, frame_num in enumerate(range_frames):
-      print(frame_num)
+      #print(frame_num)
       # obtain frame
       video.set(cv2.CAP_PROP_POS_FRAMES, frame_num) #0-based index of the frame to be decoded/captured next.
       ret, frame = video.read()
@@ -201,7 +213,7 @@ def detect(videoPath, config, config_type, metadata, single_frame=False, write=F
       instances = outputs['instances']
       pred_boxes1 = instances.pred_boxes[[idx_max]]
       # start of the lift would be where y-coordinates change drastically
-      print(pred_boxes1)
+      #print(pred_boxes1)
       y1_new = pred_boxes1.tensor.cpu().numpy()[0][1]
       pct_change = (y1_new - y1) / y1
       # if y1 decreases more than 20%(object moves upwards), then we found our starting point
@@ -218,47 +230,132 @@ def detect(videoPath, config, config_type, metadata, single_frame=False, write=F
         frame_prev = frame_num
   y1_start, lift_start, idx_max = lift_startend(find_start=True)
   y1_end, lift_end = lift_startend()
-  print('lift_start_frame: ', lift_start)
-  print('lift_end_frame: ', lift_end)
-    # pred_scores1 = instances.scores[[idx_max]]
-    # pred_classes1 = instances.pred_classes[[idx_max]]
-    # # Set new instances
-    # outputs1 = detectron2.structures.Instances(image_size=(height, width))
-    # outputs1.set('pred_boxes', pred_boxes1)
-    # outputs1.set('scores', pred_scores1)
-    # outputs1.set('pred_classes', pred_classes1)
-    # if config_type == 'keypoint':
-    #   # object detection won't have keypoint attributes
-    #   pred_keypoints1 = instances.pred_keypoints[[idx_max]]
-    #   pred_keypoints_heatmaps1 = instances.pred_keypoint_heatmaps[[idx_max]]
-    #   outputs1.set('pred_keypoints', pred_keypoints1)
-    # v = Visualizer(frame[:, :, ::-1],metadata, scale=1.2)
-    # out = v.draw_instance_predictions(outputs1.to("cpu"))
-    # print(outputs1)
-    # if write:
-    #   cv2.imwrite(f'frames/{n}.jpg', out.get_image())
-    # else:
-    #   out = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
-    #   plt.imshow(out)
-    #   plt.show()
-    # #   cv2.imshow('output',out.get_image()[:, :, ::-1])
-    # #   cv2.waitKey(0)
-    # #   cv2.destroyAllWindows()
-    #   return outputs1, v
-# %%
-# Barbell detection
-barbell_metadata = MetadataCatalog.get("barbell_test")
-detect(videoPath, cfg_obj, 'object', barbell_metadata, single_frame=True, write=False)
-#outputs.pred_keypoints[0][0]
-#cv2_imshow(img.get_image()[:, :, ::-1])
-# %%
-cfg_key = get_cfg() # obtain detectron2's default config
-cfg_key.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")) # load values from a file specified under Detectron2's official configs/ directory
-cfg_key.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml") # returns the URL to the model trained using the given config
-cfg_key.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 # threshold to filter out low-scored bounding boxes
-cfg_key.MODEL.DEVICE = 'cuda' #"cuda" # cpu or cuda
+
+  def lift_segmenter():
+    predictor = DefaultPredictor(cfg_key)
+    foi = {}
+    for i, frame_num in enumerate(range(lift_start,lift_end)):
+      # obtain frame
+      video.set(cv2.CAP_PROP_POS_FRAMES, frame_num) #0-based index of the frame to be decoded/captured next.
+      ret, frame = video.read()
+      # make predictions
+      outputs = predictor(frame)
+      # keypoint detection only looks for humans, so don't need to filter out other objects
+      # filtering by largest bounding box, which should mean object detected is closest to camera
+      idx_max = outputs['instances'].pred_boxes.area().cpu().argmax().tolist()
+      instances = outputs['instances']
+      pred_boxes1 = instances.pred_boxes[[idx_max]]
+      pred_scores1 = instances.scores[[idx_max]]
+      pred_classes1 = instances.pred_classes[[idx_max]]
+      pred_keypoints1 = instances.pred_keypoints[[idx_max]]
+      #pred_keypoints_heatmaps1 = instances.pred_keypoint_heatmaps[[idx_max]]
+      # Set new instances
+      outputs1 = detectron2.structures.Instances(image_size=(height, width))
+      outputs1.set('pred_boxes', pred_boxes1)
+      outputs1.set('scores', pred_scores1)
+      outputs1.set('pred_classes', pred_classes1)
+      outputs1.set('pred_keypoints', pred_keypoints1)
+      keypts = pred_keypoints1.cpu().numpy()[0]
+      # find initial pull
+      if 'initial_pull' not in foi:
+        # use an average of left and right wrist/knee score to get highest confidence movement analysis
+        left_wristknee_score = [keypts[9][2], keypts[13][2]]
+        left_score = np.average(left_wristknee_score)
+        right_wristknee_score = [keypts[10][2], keypts[14][2]]
+        right_score = np.average(right_wristknee_score)
+        # if scores are equivalent, use side with highest score/confidence
+        if left_score == right_score:
+          left_score = max(left_wristknee_score)
+          right_score = max(right_wristknee_score)
+        elif left_score > right_score:
+          wrist_y = keypts[9][1]
+          knee_y = keypts[13][1]
+        elif right_score > left_score:
+          wrist_y = keypts[10][1]
+          knee_y = keypts[14][1]
+        # when wrist goes above the knee, that's the end of the initial pull
+        if wrist_y < knee_y:
+          foi['initial_pull'] = frame_num
+        continue
+      if ('initial_pull' in foi) & ('power_position' not in foi):
+        left_wristhip_score = [keypts[9][2], keypts[11][2]]
+        left_score = np.average(left_wristhip_score)
+        right_wristhip_score = [keypts[10][2], keypts[12][2]]
+        right_score = np.average(right_wristhip_score)
+        # if scores are equivalent, use side with highest score/confidence
+        if left_score == right_score:
+          left_score = max(left_wristhip_score)
+          right_score = max(right_wristhip_score)
+        elif left_score > right_score:
+          wrist_y = keypts[9][1]
+          hip_y = keypts[11][1]
+        elif right_score > left_score:
+          wrist_y = keypts[10][1]
+          hip_y = keypts[12][1]
+        # when wrist goes above the knee, that's the end of the initial pull
+        if wrist_y < hip_y:
+          foi['power_position'] = frame_num
+          # storing left leg angle and right leg angle to detect catch
+          left_hipkneeankle_score = [keypts[11][2], keypts[13][2], keypts[15][2]]
+          left_score = np.average(left_hipkneeankle_score)
+          right_hipkneeankle_score = [keypts[12][2], keypts[14][2], keypts[16][2]]
+          right_score = np.average(right_hipkneeankle_score)
+          if left_score == right_score:
+            left_score = max(left_hipkneeankle_score)
+            right_score = max(right_hipkneeankle_score)
+          # use left side if score is higher
+          elif left_score > right_score:
+            hip =  np.array(keypts[11][:2])
+            knee = np.array(keypts[13][:2])
+            ankle = np.array(keypts[15][:2])
+          # use right side if score is higher
+          elif right_score > left_score:
+            hip = np.array(keypts[12][:2])
+            knee = np.array(keypts[14][:2])
+            ankle = np.array(keypts[16][:2])
+          leg_degree = keypoint_degree(hip, knee, ankle)
+        continue
+      if ('initial_pull' in foi) & ('power_position' in foi) & ('catch' not in foi):
+        left_hipkneeankle_score = [keypts[11][2], keypts[13][2], keypts[15][2]]
+        left_score = np.average(left_hipkneeankle_score)
+        right_hipkneeankle_score = [keypts[12][2], keypts[14][2], keypts[16][2]]
+        right_score = np.average(right_hipkneeankle_score)
+        if left_score == right_score:
+          left_score = max(left_hipkneeankle_score)
+          right_score = max(right_hipkneeankle_score)
+        # use left side if score is higher
+        elif left_score > right_score:
+          hip =  np.array(keypts[11][:2])
+          knee = np.array(keypts[13][:2])
+          ankle = np.array(keypts[15][:2])
+        # use right side if score is higher
+        elif right_score > left_score:
+          hip = np.array(keypts[12][:2])
+          knee = np.array(keypts[14][:2])
+          ankle = np.array(keypts[16][:2])
+        leg_degree_new = keypoint_degree(hip, knee, ankle)
+        if leg_degree_new < leg_degree:
+          leg_degree = leg_degree_new
+          # foi['catch'] = frame_num
+          print(frame_num, ' -- ', 'leg_degree: ', str(leg_degree))
+      #v = Visualizer(frame[:, :, ::-1], keypoint_metadata, scale=1.2)
+      #out = v.draw_instance_predictions(outputs1.to("cpu"))
+      #cv2.imwrite(f'frames/{frame_num}.jpg', out.get_image())
+      #   out = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
+      #   plt.imshow(out)
+      #   plt.show()
+    return foi
+  frames_of_interest = lift_segmenter()
+  print(frames_of_interest)
+  #return outputs_list
+  # output_file = open('keypoint_outputs', 'w', encoding='utf-8')
+  # for dic in outputs_list:
+  #     json.dump(dic, output_file) 
+  #     output_file.write("\n")
+#%%
+outputs = detect(videoPath)
 # %% Keypoint indices
-keypoint_names = MetadataCatalog.get(cfg_key.DATASETS.TRAIN[0]).keypoint_names
+keypoint_names = keypoint_metadata.keypoint_names
 keypoint_names.index('nose')
 # "keypoints": [
 # "nose",
